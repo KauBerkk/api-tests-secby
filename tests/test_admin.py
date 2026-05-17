6c554fcfc4d67237c714ce2548c67c3237947162
KauBerkk <unkoltan@gmail.com>
2026-05-17 18:08:20 +0300

    Добавил отдельный сценарий test_admin

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,22 +1,57 @@
 import allure
+
 from utils.api_client import ApiClient
+from utils.config import ADMIN_LOGIN, ADMIN_PASSWORD
 
 
-@allure.feature("Roles")
-def test_user_access_profiles(user_token):
-
-    response = ApiClient.get_profiles(user_token)
-    assert response.status_code in [200, 403]
-
-
-@allure.feature("Roles")
-def test_token_contains_role(test_user):
+def get_admin_token():
 
     response = ApiClient.login(
-        test_user["username"],
-        test_user["password"]
+        ADMIN_LOGIN,
+        ADMIN_PASSWORD
     )
 
-    data = response.json()
+    response_data = response.json()
 
-    assert data["user"]["role"]["name"] == "user"
+    return response_data["access_token"]
+
+
+@allure.feature("Admin")
+@allure.story("Admin login")
+def test_admin_login_success():
+
+    response = ApiClient.login(
+        ADMIN_LOGIN,
+        ADMIN_PASSWORD
+    )
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+
+    assert "access_token" in response_data
+
+
+@allure.feature("Admin")
+@allure.story("Admin role validation")
+def test_admin_role():
+
+    response = ApiClient.login(
+        ADMIN_LOGIN,
+        ADMIN_PASSWORD
+    )
+
+    response_data = response.json()
+
+    assert response_data["user"]["role"]["name"] == "admin"
+
+
+@allure.feature("Admin")
+@allure.story("Admin can access profiles")
+def test_admin_can_get_profiles():
+
+    token = get_admin_token()
+
+    response = ApiClient.get_profiles(token)
+
+    assert response.status_code == 200
